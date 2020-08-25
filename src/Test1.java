a55a8d407ae839a6367523093a984a6d29c5e4b8
Maroof <maroofkhan2396@gmail.com>
2020-08-25 14:51:26 +0530

    first commit

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+	void m(){
+		System.out.println("hello git");
+		System.out.println("secind");
+	}
+
+	public static void main(String[] args) {
+		
+		Test1 t=new Test1();
+		t.m(); 
+	}
+
+}
